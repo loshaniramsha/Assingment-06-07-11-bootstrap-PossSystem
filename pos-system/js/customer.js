f43e382384b4c27bf402cc4e9408dc7fb945fc7a
loshaniramsha <loshanikottagoda@gmail.com>
2024-05-07 12:57:21 +0530

    add js submit-button and tbl-loard

--- pos-system/js/customer.js
+++ pos-system/js/customer.js
@@ -1,5 +1,6 @@
+/*
 var customer=[];
-
+var recordIndex;
 
 $('#save-customer').on('click',()=>{
     alert("save customer")
@@ -11,30 +12,94 @@ $('#save-customer').on('click',()=>{
     console.log(customerId,customerName,customerAddress,customerSalary);
 })
 
-/*$('#save-customer').on('click', () => {
-    // Alert to indicate that the save operation is being performed
+function lordTable(){
+  $('#customer-tbl-body').empty();
+    customer.map((item, index) => {
+        let record = `<tr>
+        <td class="customer-id-value">${item.id}</td>
+        <td class="customer-name-value">${item.name}</td>
+        <td class="customer-address-value">${item.address}</td>
+        <td class="customer-salary-value">${item.salary}</td>
+        </tr>`;
+        $('#customer-tbl-body').append(record);
+    });
+}
+
+$("#customer-tbl-body").on('click',`tr`,function ()  {
+   let index=$(this).index();
+   recordIndex=index;
+   console.log("index:",index);
+
+   let id=$(this).find("customer-id-value").text();
+   let name=$(this).find("customer-name-value").text();
+   let address=$(this).find("customer-address-value").text();
+   let salary=$(this).find("customer-salary-value").text();
+
+   $("#customer_id").val(id);
+   $("#customer_name").val(name);
+   $("#customer_address").val(address);
+   $("#customer_salary").val(salary);
+
+})*/
+var customer = [];
+var recordIndex;
+
+$('#save-customer').on('click', () => {
+
     alert("Saving customer...");
 
-    // Retrieve values from input fields
+
     var customerId = $('#customer_id').val();
     var customerName = $('#customer_name').val();
     var customerAddress = $('#customer_address').val();
     var customerSalary = $('#customer_salary').val();
 
-    // Create a new row for the table
-    var newRow = `<tr>
-                    <td>${customerId}</td>
-                    <td>${customerName}</td>
-                    <td>${customerAddress}</td>
-                    <td>${customerSalary}</td>
-                  </tr>`;
 
-    // Append the new row to the table body
-    $('#customer-table tbody').append(newRow);
+    var newCustomer = {
+        id: customerId,
+        name: customerName,
+        address: customerAddress,
+        salary: customerSalary
+    };
 
-    // Log the data to the console
+
+    customer.push(newCustomer);
+
+    // Log the data to the console for verification
     console.log("Customer ID:", customerId);
     console.log("Customer Name:", customerName);
     console.log("Customer Address:", customerAddress);
     console.log("Customer Salary:", customerSalary);
-});*/
+
+
+    loadTable();
+});
+
+function loadTable() {
+    $('#customer-tbl-body').empty();
+    customer.map((item, index) => {
+        let record = `<tr>
+                        <td class="customer-id-value">${item.id}</td>
+                        <td class="customer-name-value">${item.name}</td>
+                        <td class="customer-address-value">${item.address}</td>
+                        <td class="customer-salary-value">${item.salary}</td>
+                    </tr>`;
+        $('#customer-tbl-body').append(record);
+    });
+}
+
+$("#customer-tbl-body").on('click', 'tr', function () {
+    let index = $(this).index();
+    recordIndex = index;
+    console.log("index:", index);
+
+    let id = $(this).find(".customer-id-value").text();
+    let name = $(this).find(".customer-name-value").text();
+    let address = $(this).find(".customer-address-value").text();
+    let salary = $(this).find(".customer-salary-value").text();
+
+    $("#customer_id").val(id);
+    $("#customer_name").val(name);
+    $("#customer_address").val(address);
+    $("#customer_salary").val(salary);
+});
