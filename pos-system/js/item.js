e5ea1f77482b8ff4449de84445e15318bbb1c628
loshaniramsha <loshanikottagoda@gmail.com>
2024-05-13 10:57:04 +0530

    updates item section

--- pos-system/js/item.js
+++ pos-system/js/item.js
@@ -11,6 +11,8 @@ $("#item-save").on('click', () => {
     var itemPrice = $("#item_price").val();
     var itemQty = $("#item_qty").val();
 
+    $('#close-item-model').click();
+
     var newItem = {
         code: itemCode,
         name: itemName,
@@ -68,5 +70,41 @@ $("#item-delete").on('click', () => {
         loadTable(items);
     } else {
         alert("Delete canceled");
+    }
+});
+
+
+$('#close-item-model').on('click', () => {
+    // Clear the input fields
+    $('#itemCode').val('');
+    $('#item_name').val('');
+    $('#item_price').val('');
+    $('#item_qty').val('');
+});
+
+$('#exite-item-model').on('click', () => {
+    $('#staticBackdrop-item').modal('hide');
+});
+
+$('#revew-item').on('click', () => {
+    // Get the entered item code
+    var itemCode = $('#itemCode').val();
+
+    // Find the index of the item in the array based on the entered code
+    var itemIndex = items.findIndex(i => i.code === itemCode);
+
+    // Check if the item with the entered code exists
+    if (itemIndex !== -1) {
+        // Retrieve the item details from the array
+        var selectedItem = items[itemIndex];
+
+        // Fill the input fields with the retrieved item details
+        $("#item_name").val(selectedItem.name);
+        $("#item_price").val(selectedItem.price);
+        $("#item_qty").val(selectedItem.qty);
+
+        console.log("Item details filled successfully.");
+    } else {
+        alert("Item with the entered code does not exist.");
     }
 });
